f2ae3aa54f6f91c8274b7c99a2a4235e9ae1e872
Ming-yuan <1978764+myjian@users.noreply.github.com>
2024-07-20 02:51:37 +0900

    Show UTAGE level in single play record

--- src/scripts/play-record-helper.ts
+++ src/scripts/play-record-helper.ts
@@ -1,4 +1,4 @@
-import {getChartType} from '../common/chart-type';
+import {ChartType, getChartType} from '../common/chart-type';
 import {Difficulty, getDifficultyForRecord} from '../common/difficulties';
 import {getSongName} from '../common/fetch-score-util';
 import {getGameRegionFromOrigin, isMaimaiNetOrigin} from '../common/game-region';
@@ -19,16 +19,20 @@ import {fetchSongDetailPage} from '../common/util';
     const songImg = d.querySelector('img.music_img') as HTMLImageElement;
     const genre = name === 'Link' && isNiconicoLinkImg(songImg.src) ? 'niconico' : '';
     const chartType = getChartType(d.body);
-    const props = songDb.getSongProperties(name, genre, chartType);
-    if (props) {
-      return getDisplayLv(props.lv[diff]);
+    if (chartType != ChartType.UTAGE) {
+      const props = songDb.getSongProperties(name, genre, chartType);
+      if (props) {
+        return getDisplayLv(props.lv[diff]);
+      }
     }
 
     // If magic does not work, load from maimai-NET.
     const songIdxElem = d.querySelector('input[name=idx]') as HTMLInputElement;
     const songDetailPage = await fetchSongDetailPage(songIdxElem.value);
     const lvElem = songDetailPage.querySelector(
-      `.music_detail_table tr:nth-child(${diff + 1}) .music_lv_back`
+      `.music_detail_table tr:nth-child(${
+        chartType == ChartType.UTAGE ? 1 : diff + 1
+      }) .music_lv_back`
     );
     return lvElem.innerHTML.trim();
   }
